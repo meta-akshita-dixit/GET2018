87dbd4e2e32a83c60d84bbf3ba7be265148014c9
meta-akshita-dixit <35727425+meta-akshita-dixit@users.noreply.github.com>
2018-01-30 15:02:46 +0530

    Updated
    Removed the global variables
    Removed the use of annotation before
    added more test cases

--- PF-session1/assignment3/TestLongestSequence.java
+++ PF-session1/assignment3/TestLongestSequence.java
@@ -7,19 +7,54 @@ import org.junit.Test;
 
 public class TestLongestSequence 
 {
-	LongestSequence test;
-	int arr[],expectedArr[];
-	@Before
-	public void toBegin()
-	{
+	
+	@Test
+	public void testSuccess() {
+		LongestSequence test;
+		int arr[],expectedArr[];
 		arr=new int[]{1,2,3,2,3,4,5,3,4,2,2,3,4,5,6,7,8,1,2,4,5,6,7,8,9};
 		test=new LongestSequence();
 		expectedArr=new int[]{1,2,4,5,6,7,8,9};
-	}
-	@Test
-	public void test() {
 		assertArrayEquals(expectedArr,test.longestSequence(arr));
 	}
 
-}
+	@Test
+	public void testFailure() {
+		LongestSequence test;
+		int arr[],expectedArr[];
+		arr=new int[]{1,2,3,2,3,4,5,3,4,2,2,3,4,5,6,7,8,1,2,4,5,6,7,8,9};
+		test=new LongestSequence();
+		expectedArr=new int[]{1,2,4,5,6,7,9};
+		assertNotEquals(expectedArr,test.longestSequence(arr));
+	}
 
+	@Test
+	public void testSuccessEmpty() {
+		LongestSequence test;
+		int arr[],expectedArr[];
+		arr=new int[]{};
+		test=new LongestSequence();
+		expectedArr=new int[]{};
+		assertArrayEquals(expectedArr,test.longestSequence(arr));
+	}
+	
+	@Test
+	public void testSuccessEqualElements() {
+		LongestSequence test;
+		int arr[],expectedArr[];
+		arr=new int[]{1,1,1,1,1,1};
+		test=new LongestSequence();
+		expectedArr=new int[]{1};
+		assertArrayEquals(expectedArr,test.longestSequence(arr));
+	}
+	
+	@Test
+	public void testSuccessNegativeElements() {
+		LongestSequence test;
+		int arr[],expectedArr[];
+		arr=new int[]{-1,-3,-2,0,1};
+		test=new LongestSequence();
+		expectedArr=new int[]{-3,-2,0,1};
+		assertArrayEquals(expectedArr,test.longestSequence(arr));
+	}
+}
