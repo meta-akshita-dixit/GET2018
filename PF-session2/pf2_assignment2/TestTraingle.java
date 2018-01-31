8f1b737c49373fb136a962524cd493519a3f085c
meta-akshita-dixit <35727425+meta-akshita-dixit@users.noreply.github.com>
2018-01-31 10:24:50 +0530

    Update TestTraingle.java

--- PF-session2/pf2_assignment2/TestTraingle.java
+++ PF-session2/pf2_assignment2/TestTraingle.java
@@ -2,50 +2,42 @@ package pf2_assignment2;
 
 import static org.junit.Assert.*;
 
-import org.junit.Before;
 import org.junit.Test;
 
 
 public class TestTraingle {
 
-	Triangle t = new Triangle();
-	String expected[];
-	String number, space;
-	@Before
-	public void beforePattern(){
-		expected = new String[]{"12345", " 1234", "  123", "   12", "    1"};
-		
-	}
 	
-
 	@Test
 	public void testPattern() {
+		   Triangle t = new Triangle();
+		   String expected[];
+		   expected = new String[]{"12345", " 1234", "  123", "   12", "    1"};
 		   int n=5;
 		   assertArrayEquals(expected,t.triangle(n));
 	}
-	@Before
-	public void beforeNumbers(){
-		number = new String("123");
-	}
 	
-
 	@Test
 	public void testNumbers() {
+		   Triangle t = new Triangle();
 		   int n=5;
+		   String number;
+		   number = new String("123");
 		   assertEquals(number,t.numbers(3, n));
 	}
-	@Before
-	public void beforeSpaces(){
-		space = new String("  ");
-	}
-	
 
 	@Test
 	public void testSpaces() {
 		   int n=5;
+		   Triangle t = new Triangle();
+		   String space;
+		   space = new String("  ");
 		   assertEquals(space,t.spaces(3, n));
 	}
 
 }
+
+
+
 
 
