86cd6304c950ea0642c0ce52856c6b06ba6c6d00
meta-akshita-dixit <35727425+meta-akshita-dixit@users.noreply.github.com>
2018-01-31 10:13:12 +0530

    Update TestPyramid.java

--- PF-session2/pf2_assignment1/TestPyramid.java
+++ PF-session2/pf2_assignment1/TestPyramid.java
@@ -7,41 +7,38 @@ import org.junit.Test;
 
 public class TestPyramid {
 	
-	Pyramid p = new Pyramid();
-	String expected[];
-	String number, space;
-	@Before
-	public void beforePattern(){
-		expected = new String[]{"    1","   121","  12321"," 1234321","123454321"," 1234321","  12321","   121","    1"};
-		
-	}
 	
-
 	@Test
 	public void testPattern() {
+		   Pyramid p = new Pyramid();
 		   int n=5;
+		   String expected[];
+		   expected = new String[]{"    1","   121","  12321"," 1234321","123454321"," 1234321","  12321","   121","    1"};
 		   assertArrayEquals(expected,p.pyramid(n));
 	}
-	@Before
-	public void beforeNumbers(){
-		number = new String("123454321");
-	}
+
 	
 
 	@Test
 	public void testNumbers() {
+		   Pyramid p = new Pyramid();
+		   String number;
 		   int n=5;
+		   number = new String("123454321");
 		   assertEquals(number,p.numbers(5, n));
 	}
 	@Before
 	public void beforeSpaces(){
-		space = new String("    ");
+		
 	}
 	
 
 	@Test
 	public void testSpaces() {
 		   int n=5;
+		   Pyramid p = new Pyramid();
+		   String space;
+		   space = new String("    ");
 		   assertEquals(space,p.spaces(1, n));
 	}
 
