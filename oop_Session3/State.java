cf6d03757bd5b42305f5e346b6601d0f69fdfe3f
meta-akshita-dixit <35727425+meta-akshita-dixit@users.noreply.github.com>
2018-02-05 15:07:10 +0530

    Add files via upload
    OOP assignment 3

---
+++ oop_Session3/State.java
@@ -0,0 +1,21 @@
+package oop_Session3;
+
+/**
+ * 
+ * @author Akshita Dixit
+ * This class represents the various state of workflow
+ */
+public class State {
+	
+	//variable storing name of the workflow
+	String stateName;
+	
+	/**
+	 * constructor initializing statename
+	 * @param stateName
+	 */
+	State(String stateName){
+		this.stateName = stateName;
+	}
+	
+}
