91a636be6982d21a1cd50a58bd04bf96fbe53b98
meta-akshita-dixit <35727425+meta-akshita-dixit@users.noreply.github.com>
2018-02-07 14:57:59 +0530

    Update Span.java

--- oop_Session1/Span.java
+++ oop_Session1/Span.java
@@ -1,17 +1,22 @@
 package oop_Session1;
 
 /**
- * 
- * @author Akshita Dixit
  * Span is a HTML tag
  * Span is type of Composite Element
+ * @author Akshita Dixit
+ * 
  */
 public class Span extends CompositeElement{
 	
-	// Constructor to initialize the values
+	/**
+	 * Constructor for initializing values
+	 * @param id - id of the tag
+	 * @param className - name of the class of the tag
+	 */
 	Span(String id, String className){
 		
 		// calling parent class for initialization
 		super(id , className);
 	}
 }
+
