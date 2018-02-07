3628ec6bb2888a72750b68c65c28009a71c2b268
meta-akshita-dixit <35727425+meta-akshita-dixit@users.noreply.github.com>
2018-02-07 14:56:51 +0530

    Update Div.java

--- oop_Session1/Div.java
+++ oop_Session1/Div.java
@@ -1,14 +1,19 @@
 package oop_Session1;
 
 /**
- * 
- * @author Akshita Dixit
- * Div is a HTML tag
+ *  Div is a HTML tag
  * Div is type of Composite Element
+ * @author Akshita Dixit
+
  */
 public class Div extends CompositeElement {
 	
-	// Constructor to initialize the values
+	/**
+	 * Constructor to initialize the values
+	 * @param id - id of the tag
+	 * @param className - name of the class of the tag
+	 * @return
+	 */
 	Div(String id, String className){
 		
 		// calling parent class for initialization
