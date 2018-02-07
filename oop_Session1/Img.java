0521c18e8aa1914a30d777b962595f0a674dc2b9
meta-akshita-dixit <35727425+meta-akshita-dixit@users.noreply.github.com>
2018-02-07 14:57:29 +0530

    Update Img.java

--- oop_Session1/Img.java
+++ oop_Session1/Img.java
@@ -1,7 +1,6 @@
 package oop_Session1;
 
 /**
- * 
  * @author Akshita Dixit
  * Image is a HTML tag, in this we denote Image as "img"
  * Image is type of Atomic Element
@@ -12,7 +11,12 @@ public class Img extends AtomicElement {
 	// Attribute of Image tag
 	String src;
 	
-	// Constructor to initialize the values
+	/**
+	 * Constructor to initialize the values
+	 * @param id of the tag
+	 * @param src source of image
+	 * @param className of the tag
+	 */
 	Img(String id, String src, String className){
 		
 		// calling parent class for initialization
@@ -20,6 +24,5 @@ public class Img extends AtomicElement {
 		
 		// initializing the value
 		this.src = src;
-		
 	}
 }
