838439bd3fa4dc63989803dd5c798e1434fa02aa
meta-akshita-dixit <35727425+meta-akshita-dixit@users.noreply.github.com>
2018-03-05 17:13:54 +0530

    Update script.js

--- JSS Session 2/assignment 2/script.js
+++ JSS Session 2/assignment 2/script.js
@@ -10,29 +10,30 @@ function removeDuplicate() {
 	//variable to store the output string returned by method remove
 	var outputString = remove(inputString);
 	//to get tag using id
-    var tag = document.getElementById('print');
-    tag.innerHTML = outputString;
+	var tag = document.getElementById('print');
+	tag.innerHTML = outputString;
 }
 
 /**
  *method to remove continuous duplicates from the string
  *param-string having duplicates
+ *return-returns updated string
  */
 function remove(string) {
-	//variable to count the position in the string
+    //variable to count the position in the string
     var count = 0;
-	//loop to check characters of the strings
+    //loop to check characters of the strings
     for(i = 0;i < string.length; i++) {
         if(string[i] == string[i+1]) {
-			count++;
-			continue;
-		}
-		if(count > 0) {
-			string = string.slice(0 ,i - count) + string.slice(i + 1 , string.length);
-			count = 0;
-			//to check if the new optimized string has duplicates
-			string = remove(string);
-		}
+		count++;
+		continue;
+	}
+	if(count > 0) {
+		string = string.slice(0 ,i - count) + string.slice(i + 1 , string.length);
+		count = 0;
+		//to check if the new optimized string has duplicates
+		string = remove(string);
+	}
 	}
   return string;
 }
