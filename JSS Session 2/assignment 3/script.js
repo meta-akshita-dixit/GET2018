59f7182971f9942433f545ff953e48e340a5e250
meta-akshita-dixit <35727425+meta-akshita-dixit@users.noreply.github.com>
2018-03-05 17:17:05 +0530

    Update script.js

--- JSS Session 2/assignment 3/script.js
+++ JSS Session 2/assignment 3/script.js
@@ -11,16 +11,16 @@ var rear = -1;
  *method to enqueue new element in the queue
  */
 function enqueue() {
-  var data = document.getElementById('data').value;
-  if(data != "" && rear <= 100) {
+    var data = document.getElementById('data').value;
+    if(data != "" && rear <= 100) {
     if(front == -1 && rear == -1) {
-      front = 0;
-      rear = 0;
+	  front = 0;
+	  rear = 0;
     } else {
-      rear++;
+          rear++;
     }
     queue[rear] = data;
-	document.getElementById('data').value="";
+    document.getElementById('data').value="";
   } else { //error in adding new element
     alert("Element can't be enqueued either because of data is empty or queue is full");
   }
@@ -29,21 +29,20 @@ function enqueue() {
  *method to dequeue an element from the queue
  */
 function dequeue() {
-  if(front == rear+1) {
-    alert("queue is empty");
-  } else {
-    data = queue[front++];
-    document.getElementById('show').innerHTML = data;
+    if(front == rear+1) {
+      alert("queue is empty");
+    } else {
+      data = queue[front++];
+      document.getElementById('show').innerHTML = data;
+    }
   }
-}
 /**
  *method to show the elements of the queue
  */
 function showQueue() {
-	document.getElementById('show').innerHTML = "";
-	//loop to display the elements
-	for(var loopCounter = front; loopCounter <= rear; loopCounter++){
+     document.getElementById('show').innerHTML = "";
+     //loop to display the elements
+     for(var loopCounter = front; loopCounter <= rear; loopCounter++){
 		document.getElementById('show').innerHTML += queue[loopCounter]+"   ";
-	}
-	
+     }
 }
