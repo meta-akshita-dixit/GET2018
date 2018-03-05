abd6fe080ab31306bce6ca5e0d489c0799e73840
meta-akshita-dixit <35727425+meta-akshita-dixit@users.noreply.github.com>
2018-03-05 17:20:05 +0530

    Update script.js

--- JSS Session 2/assignment 4/script.js
+++ JSS Session 2/assignment 4/script.js
@@ -9,32 +9,33 @@ var topElement = -1;
  *method to push new element in the stack
  */
 function push() {
-  document.getElementById('show').innerHTML = "";
-  //if stack is full
-  if(topElement < 100){
-    var data = document.getElementById('data').value;
-    topElement = topElement + 1;
-    stack[topElement] = data;
-  } else{
-    alert("stack is full");
-  }
-  document.getElementById('data').value = "";
-}
+	
+	document.getElementById('show').innerHTML = "";
+	//if stack is full
+        if(topElement < 100){
+		var data = document.getElementById('data').value;
+        	topElement = topElement + 1;
+   		stack[topElement] = data;
+	} else{
+	   	alert("stack is full");
+	}
+        document.getElementById('data').value = "";
+   }
 
 /**
  *method to pop an element from the stack
  */
 function pop(){
-  document.getElementById('show').innerHTML = "";
-  //if stack is not empty
-  if(topElement > -1){
-    var data = stack[topElement];
-    topElement = topElement - 1;
-    document.getElementById('show').innerHTML = data;
-  } else{
-    document.getElementById('show').innerHTML = "";
-    alert("no element in stack");
-  }
+  	document.getElementById('show').innerHTML = "";
+	//if stack is not empty
+	if(topElement > -1){
+		var data = stack[topElement];
+		topElement = topElement - 1;
+		document.getElementById('show').innerHTML = data;
+	} else{
+		document.getElementById('show').innerHTML = "";
+		alert("no element in stack");
+	}
 }
 
 function showStack() {
