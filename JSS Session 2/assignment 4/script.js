eb3948cf1dfbbf9981f51d4379bbff620aed5f66
meta-akshita-dixit <35727425+meta-akshita-dixit@users.noreply.github.com>
2018-03-06 10:36:38 +0530

    Update script.js

--- JSS Session 2/assignment 4/script.js
+++ JSS Session 2/assignment 4/script.js
@@ -10,39 +10,39 @@ var topElement = -1;
  */
 function push() {
 	
-	document.getElementById('show').innerHTML = "";
+	document.getElementById("show").innerHTML = "";
 	//if stack is full
-        if(topElement < 100){
-		var data = document.getElementById('data').value;
+        if(topElement < 100) {
+		var data = document.getElementById("data").value;
         	topElement = topElement + 1;
    		stack[topElement] = data;
-	} else{
+	} else {
 	   	alert("stack is full");
 	}
-        document.getElementById('data').value = "";
+        document.getElementById("data").value = "";
    }
 
 /**
  *method to pop an element from the stack
  */
-function pop(){
-  	document.getElementById('show').innerHTML = "";
+function pop() {
+  	document.getElementById("show").innerHTML = "";
 	//if stack is not empty
-	if(topElement > -1){
+	if(topElement > -1) {
 		var data = stack[topElement];
 		topElement = topElement - 1;
-		document.getElementById('show').innerHTML = data;
-	} else{
-		document.getElementById('show').innerHTML = "";
+		document.getElementById("show").innerHTML = data;
+	} else {
+		document.getElementById("show").innerHTML = "";
 		alert("no element in stack");
 	}
 }
 
 function showStack() {
-	document.getElementById('show').innerHTML = "";
+	document.getElementById("show").innerHTML = "";
 	//loop to display the elements
-	for(var loopCounter = topElement; loopCounter >= 0; loopCounter--){
-		document.getElementById('show').innerHTML += stack[loopCounter]+"   ";
+	for(var loopCounter = topElement; loopCounter >= 0; loopCounter--) {
+		document.getElementById("show").innerHTML += stack[loopCounter] + "   ";
 	}
 	
 }
